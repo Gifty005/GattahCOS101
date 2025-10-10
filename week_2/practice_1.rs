6f38a6f60ec2a4c950167f6d746b9df0025d3b50
Gifty005 <gift.attah@pau.edu.ng>
2025-10-10 11:16:15 +0100

    (my second commit)

---
+++ week_2/practice_1.rs
@@ -0,0 +1,13 @@
+fn main(){
+	println!("welcome to COS 101");
+	println!("the course learning outcome is as follows:",);
+
+	println!("1.Distinguish between computer and computer programming,/n
+	2.Develop some techniques in computer science,/n
+	3.Understand the different areas in computer science,/n
+	4.Conversant with the application of computer science,/n
+	5.Navigate the career prospect in computer science,/n
+	6.Conversant with computer programming concepts.");
+}
+
+
---
+++ week_2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!(); // print just a
+
+	println!("hello");// print
+
+	println!("format {} arguments", "some"); //print format some arguments
+
+	println!("my name is{}. Iam the class rep of stream 1", "Attah Eleojo Gift");
+}
