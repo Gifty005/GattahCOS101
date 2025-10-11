e29c5e1c35575974335ed3699905e90d245a0a60
Gifty005 <gift.attah@pau.edu.ng>
2025-10-11 14:28:24 +0100

    (my first project)

---
+++ week_2/project_2.rs
@@ -0,0 +1,14 @@
+fn main(){
+	let t :f64 =450000.00;
+	let m :f64 =1500000.00;
+	let h :f64 =750000.00;
+	let d :f64 =2850000.00;
+	let a :f64 =250000.00;
+
+	//sum
+	let s = t + m + h + d + a;
+	let q =2.0+1.0+3.0+3.0+1.0;
+	println! ("sum is {}",s);
+	let a = s/q;
+	println!("average is {}",a);
+}
