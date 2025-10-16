ac72dc7dc7117025c6d0b3caef53dfced97e6f9e
Gifty005 <gift.attah@pau.edu.ng>
2025-10-16 14:50:45 +0100

    (practice_10)

--- week_3/practice_10/src/main.rs
+++ week_3/practice_10/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     //addition
-    let sum = 5550 + 7310
+    let sum = 5550 + 7310;
     println!("The sum of 5550 and 7310 {}", sum);
 
     //subtraction
-    let difference:u32 = 95.5 - 4.3;
+    let difference:f32 = 95.5 - 4.3;
     println!("The difference of 95.5 and 4.3 {}",difference);
 
     //multiplication
-    let product:f32 =4 * 30
+    let product:f32 =4.0 * 30.0;
     println!("The multiplication of 4 and 30 = {}", product);
 
     //division
@@ -16,6 +16,6 @@ fn main() {
     println!("The division of 56.7 and 32.2 = {}",quotient);
 
     //remainder
-    let remainder = 43 % 5
+    let remainder = 43 % 5;
     println!("The remainder of 43 and 5 = {}",remainder);
 }
---
+++ week_3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\GattahCOS101\week_3\practice_10\target\debug\practice_10.exe: C:\Users\New\Documents\GattahCOS101\week_3\practice_10\src\main.rs
