5fc9393410d5e8c1d6fa8064879f525f895965e2
Gifty005 <gift.attah@pau.edu.ng>
2025-10-16 14:17:27 +0100

    (practice_6)

--- week_3/practice_6/src/main.rs
+++ week_3/practice_6/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let interest:f64 = 8;  //interger assigned to float variable
+    let interest:f64 = 8.0;  //interger assigned to float variable
     println!("interest is {}",interest);
 }
