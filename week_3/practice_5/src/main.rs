82e8f209110947f35e731af3ad1359fb433903d3
Gifty005 <gift.attah@pau.edu.ng>
2025-10-16 06:20:05 +0100

    (practice_3)

---
+++ week_3/practice_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let special_character = '@';  //default
+    let alphabet :char = 'B';
+    let surname = "Attah";
+    let first_name = "Gift";
+    let middle_name = "Eleojo"
+
+    println!("");
+    println!("Special character is: {}", special_character);
+    println!("Alphabet is: {}",alphabet );
+    println!("Name: {} {} {}",surname, middle_name,first_name);
+
+}
---
+++ week_3/practice_6/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let interest:f64 = 8;  //interger assigned to float variable
+    println!("interest is {}",interest);
+}
