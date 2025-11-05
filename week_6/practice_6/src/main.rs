3a6dfd2e21f12e2a9dbbc40d83ccd9802efd7ac5
Gifty005 <gift.attah@pau.edu.ng>
2025-11-05 16:53:28 +0100

    (practice_6)

--- week_6/practice_6/src/main.rs
+++ week_6/practice_6/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let n1 = "Electrical".to_string();
+    let n2 = "Electronic".to_string();
+    let n3 = "Engineering".to_string();
+    let n4 = n1 + &n2 + &n3; //n2 & n3 referenced is passed
+
+    // About Electrical/Electronic
+    println!("\nThe {} is informed by the aspiration to train electrical/electronic engineering proffessionals 
+        in the areas of design,building and maintenance of electrical control systems,", n4);
+
+    let w1 = "computer".to_string();
+    let w2 = "Science" .to_string();
+    let w3 = w1 + &w2; //w2 reference is passed
+    println!();
+    println!("{} is aimed at developing competent ,creative,
+        innovative,enterpreneurial and ethically-minded persons,
+        capable of creating value in the diverse fields of computer science.", w3 );
 }
