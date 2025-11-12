59db12ecabf9fa688fc2301740845c95a2c85e5d
Gifty005 <gift.attah@pau.edu.ng>
2025-11-12 19:16:16 +0100

    (practice_8)

--- week_7/practice_8/src/main.rs
+++ week_7/practice_8/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let city_arr:[&str;5] = ["Abuja","Porthacourt","Maiduguri","Kano","Lagos"];
-    println!("array is {}",city_arr );
-    println!("array size is :{}",city_arr.len() );
+    println!("array is {:?}",city_arr );
+    println!("array size is :{:?}",city_arr.len() );
 
     for index in 0..5{
         println!("city index {} is located in : {}",index,city_arr[index] );
