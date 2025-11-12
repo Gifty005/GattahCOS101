e1f8637bf6aba03f616caa0b4c816868b058b1d2
Gifty005 <gift.attah@pau.edu.ng>
2025-11-12 22:12:38 +0100

    (practice_8)

--- week_7/project/src/main.rs
+++ week_7/project/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::io::write;
+use std::f64::consts::PI;
 fn main() {
-    println!("Hello, world!");
-}
+    loop{
+        println!("\nselect a shape to calculate its area or volume:");
+        println!("1. Trapezium (Area)");
+        println!("2. Rhombus (Area)");
+        println!("3. Parallelogram(Area)");
+        println!("4. Cube(Area)");
+        println!("5.Cylinder (volume");
+        println!("6. Exit");
+
+        print!("Enter your choice (1 - 6):");
+        io::stdout().flush().unwrap();//ensure the prompt is displayed
+        
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Failed to read input");
+
+        
+
