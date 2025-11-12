f15d6e35bd8e58ccd9fe29fc8ac1643f0f47e1a7
Gifty005 <gift.attah@pau.edu.ng>
2025-11-12 18:18:14 +0100

    (practice_2)

--- week_7/practice_2/src/main.rs
+++ week_7/practice_2/src/main.rs
@@ -1,19 +1,22 @@
-fn checker(){
+use std::io;
+fn main(){
+
     let mut input = String::new();
     println!("Enter a character:");
     io::stdin().read_line(&mut input).expect("failed to read input");
     let ch:char = input.trim().parse().expect("invalid input");
+    
     if ch >= '0' && ch <= '9'
-
     {println!("character '{}' is a digit ",ch);}
     else {
         println!("character '{}' is not a digit",ch);
     }
 
-}
-fn fn main() {
+
+fn checker() {
 //calling a function
-println!("welcome! This program checks wether a character variable contains a digit or not");
+println!("welcome! This program checks wether
+ a character variable contains a digit or not");
 checker()
 }
 }
--- week_7/practice_7/src/main.rs
+++ week_7/practice_7/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    //Array with data type (expicit integer datatype)
+    let arr1:[i32; 4] = [10,20,30,40];
+    println!("\nArray with data type ");
+    println!("array is {:?}", arr1);
+    println!("array size is :{}",arr1.len() );
+
+    //Array without data type (impilicit float datatype)
+    let arr2 = [10.4, 20.7, 30.4, 40.9,51.2, 72.2];
+    println!("\nArray without datatype");
+    println!("array is {:?}",arr2 );
+    println!("array size is :{}",arr2.len() );
+
+    //Array with default values that creates and 
+    // initializes all its elements with a default value of -1.
+    let arr3:[-1;8];
+    println!("\n Array with default values");
+    println!("array is {}",arr3 );
+    println!("array size is : {}",arr3.len() );
 }
