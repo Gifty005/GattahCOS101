e872dda290735abe0a971abfa192e626a1692256
Gifty005 <gift.attah@pau.edu.ng>
2025-11-19 21:32:19 +0100

    (initializing and accessing elements)

--- week_8/practice_7/src/main.rs
+++ week_8/practice_7/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    //initialization of turple with data type
+    let datatype_tuple: (&str , f32, u8) = ("Rust",3.14,100);
+    println!("Tuple contents = {:?}", datatype_tuple );
+
+    //initialization of turple without data type
+    let no_datatype_tuple = ("Rust","fun",100);
+    println!("Tuple contents = {:?}", no_datatype_tuple );
+
+    //accessing tuple elements at index 0
+    println!("Value at Index 0 = {}", datatype_tuple.0 );
+    //accessing turple elements at index 1
+    println!("Value at index 1 = {}", datatype_tuple.1);
+
+    //accessing tuple elements at index 2
+    println!("Value at Index 2 = {}", datatype_tuple.2 );
 }
