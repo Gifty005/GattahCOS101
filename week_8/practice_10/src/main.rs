9528687cf0b2d8c7f433472307666225235172ce
Gifty005 <gift.attah@pau.edu.ng>
2025-11-20 12:16:10 +0100

    (mutable slice in rust)

--- week_8/practice_10/src/main.rs
+++ week_8/practice_10/src/main.rs
@@ -1,11 +1,12 @@
-fn main() {
-    let b : (i32,bool,f64) = (30,true,4.9);
-    print(b);
-}
-fn print (x:(i32,bool,f654)){
+fn main(){
 
-    println!("Inside print method");
-    //assigns a tuple to  distinct variables
-    let (age, is_Male,cgpa) = x;
-    println!("Age is {}, is_Male? {},  cgpa is {}"age,is_Male,cgpa );
+    let b:(i32,bool,f64) = (110,true,10.9);
+    print!("b");
 }
+
+//pass the tuple as aparameter
+
+fn print(x: (i32,bool,f64)) {
+
+   println!("Inside print method");
+    println!("{:?}",x ); }
