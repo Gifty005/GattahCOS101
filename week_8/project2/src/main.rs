4506e9689fc241074f5642bec45903d5fdd4f7c1
Gifty005 <gift.attah@pau.edu.ng>
2025-11-22 19:14:43 +0100

    (Progeam to find the person with the highest years of programming experience)

--- week_8/project2/src/main.rs
+++ week_8/project2/src/main.rs
@@ -1,3 +1,19 @@
+
 fn main() {
-    println!("Hello, world!");
+    //create alist of candidates
+    let candidates: Vec<(String, u32)> = vec![
+    ("Alice".to_string(), 5), //
+    ("Gift".to_string(), 8),
+    ("Moyo".to_string(), 3),
+    ("Ezekiel".to_string(), 8),  //same max to showtie handling
+    ];
+    //find the candidate with the highest experience
+    // 'max_by_key returns the first element with the greatest key.
+    let best = candidates
+    .iter()
+    .max_by_key(|(_, years)| *years)
+    .expect("The candidates list is empty");
+    //print the top  candidate
+    println!("Top candidate: {}({} years of programming experience)",
+    best.0, best.1 );
 }
