db7f7d2c03cfd900cf50bc84740d38157354cf4f
Gifty005 <gift.attah@pau.edu.ng>
2025-11-30 23:20:22 +0100

    Afiles that stores drinks for the Nigerian bewelry limited

--- week_9/project1/src/main.rs
+++ week_9/project1/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> io::Result <()> {
+    let lager = vec!["33 Export", "Desperados", "Goldberg", "Gulder", "Heineken", "Star"];
+    let stout = vec!["Legend","Turbo king","Williams"];
+    let non_acoholic=vec!["Maltina","Amstel malt", "Malta Gold", "Fayrouz"];
+
+let mut file = File::create("drinks.txt")?;
+let _ =writeln!(file, "Lager:");
+for drinks in &lager{
+   let _ = writeln!(file, "- {}",drinks );
+};
+ let _ =writeln!(file ,"\nStout:");
+for drinks in &stout{
+   let _ = writeln!(file, "- {}", drinks);
+}
+let _ = writeln!(file ,"\nNon-acoholic:");
+for drinks in &non_acoholic{
+  let _ =  writeln!(file, " - {}",drinks);
+}
+println!("Data written to drinks.txt");
+
+Ok (())
 }
---
+++ week_9/project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\GattahCOS101\week_9\project1\target\debug\project1.exe: C:\Users\New\Documents\GattahCOS101\week_9\project1\src\main.rs
